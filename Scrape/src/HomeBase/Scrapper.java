98a403feecd92b43edd0fa1da77ef7af8d4ce969
Unknown <jal2g15@soton.ac.uk>
2017-06-23 15:04:55 +0100

    whoops misseed one

--- Scrape/src/HomeBase/Scrapper.java
+++ Scrape/src/HomeBase/Scrapper.java
@@ -217,7 +217,7 @@ public class Scrapper extends Application {
 		System.out.println(orderNumber);
 		originalCustomerOrderNumber = html.substring(html.indexOf("CUSTOMER ORDER") + 90, html.indexOf("<", html.indexOf("CUSTOMER ORDER")+90));
 		System.out.println(originalCustomerOrderNumber);
-		transactionDate = html.substring(html.indexOf("DELIVER BY") + 86, html.indexOf("<", html.indexOf("DELIVER BY")+86));
+		transactionDate = html.substring(html.indexOf("DELIVER BY") + 85, html.indexOf("<", html.indexOf("DELIVER BY")+85));
 		System.out.println(transactionDate);
 		
 		return null;
