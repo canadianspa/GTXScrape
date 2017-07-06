461df6ba3f0e62d9e283a0288150ccbfaba9ad8f
Unknown <jal2g15@soton.ac.uk>
2017-07-06 11:51:23 +0100

    renamed scraper

--- Scrape/src/BNQ/BNQMultiScraper.java
+++ Scrape/src/BNQ/BNQMultiScraper.java
@@ -1,4 +1,4 @@
-package InvoiceScrape;
+package BNQ;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -30,7 +30,7 @@ import javafx.concurrent.Task;
 import javafx.concurrent.Worker;
 import javafx.concurrent.Worker.State;
 
-public class multiScraper extends Application {
+public class BNQMultiScraper extends Application {
 
 	ConcurrentHashMap<Integer,Integer> tabWebPos = new ConcurrentHashMap<Integer,Integer>();
 	ConcurrentHashMap<Integer,Integer> page = new ConcurrentHashMap<Integer,Integer>();
@@ -42,7 +42,8 @@ public class multiScraper extends Application {
 
 	int lastPage =1;
 	int lastPlace = 1;
-	CopyOnWriteArrayList<Invoices> listOfInvoices = new CopyOnWriteArrayList<Invoices>();
+	int seqNo = 1;
+	CopyOnWriteArrayList<EDA> listOfEda = new CopyOnWriteArrayList<EDA>();
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -51,6 +52,7 @@ public class multiScraper extends Application {
 		List<String> s = p.getRaw();
 		lastPage = Integer.parseInt(s.get(0));
 		lastPlace = Integer.parseInt(s.get(1)) - 1;
+		seqNo = Integer.parseInt(s.get(2));
 		TabPane tabPane = new TabPane();
 
 		for(int i = 0;i <10;i++)
@@ -81,7 +83,7 @@ public class multiScraper extends Application {
 				finished.put(i, false);
 
 			}
-			
+
 
 
 		}
@@ -110,10 +112,18 @@ public class multiScraper extends Application {
 					}
 					if(ready)
 					{
-						System.out.println("Adding Invoices" + listOfInvoices.size());
-						System.out.println("here");
-						InvoicesNewFunctions.addToInvoiceList((listOfInvoices));
-						InvoicesNewFunctions.createInvoices();
+						Collections.sort(listOfEda, new Comparator<EDA>() {
+
+							@Override
+							public int compare(EDA o1, EDA o2) {
+								return Integer.parseInt(o2.purchOrderNo)-Integer.parseInt(o1.purchOrderNo);
+							}
+					       
+					    });
+						EDANewFunctions.createAllXLS(listOfEda);
+						EDANewFunctions.addToEDAList(listOfEda);
+						EDANewFunctions.fillOut();
+						System.out.println(listOfEda.size() + " BNQ orders");
 						return null;
 					}
 				}
@@ -174,7 +184,7 @@ public class multiScraper extends Application {
 							}
 
 							try {
-								engine.load("https://gxstradeweb.gxsolc.com/edi-bin/EdiMailbox.pl?NextDocListed=Next&LastStartNum=" + (((page.get(place) -2)*10)+1) + "&box_type=sent&lang=en&sort_var=");
+								engine.load("https://gxstradeweb.gxsolc.com/edi-bin/EdiMailbox.pl?NextDocListed=Next&LastStartNum=" + (((page.get(place) -2)*10)+1) + "&box_type=in&lang=en&sort_var=");
 
 								tabWebPos.put(place,4);
 							} catch (Exception e) {
@@ -228,20 +238,20 @@ public class multiScraper extends Application {
 							if(isBNQ(html))
 							{
 								try {
-									Invoices myEDA = multiScraper.this.readInvoices(html);
+									EDA myEDA = BNQMultiScraper.this.readBNQHTML(html);
 									//make sure havent already seen it
 									boolean seenBefore =false;
-									for(Invoices e : listOfInvoices)
+									for(EDA e : listOfEda)
 									{
-										if(e.purchOrNo.equals(myEDA.purchOrNo))
+										if(e.purchOrderNo.equals(myEDA.purchOrderNo))
 										{
 											seenBefore = true;
 										}
 									}
 									if(!seenBefore)
 									{
-										listOfInvoices.add(myEDA);
-										System.out.println(listOfInvoices.size());
+										listOfEda.add(myEDA);
+										System.out.println(listOfEda.size());
 									}
 								} catch (Exception e) {
 									e.printStackTrace();
@@ -261,7 +271,7 @@ public class multiScraper extends Application {
 							{
 								tabWebPos.put(place,4);
 
-								engine.load("https://gxstradeweb.gxsolc.com/edi-bin/EdiMailbox.pl?NextDocListed=Next&LastStartNum=" + (((page.get(place) -2)*10)+1) + "&box_type=sent&lang=en&sort_var=");
+								engine.load("https://gxstradeweb.gxsolc.com/edi-bin/EdiMailbox.pl?NextDocListed=Next&LastStartNum=" + (((page.get(place) -2)*10)+1) + "&box_type=in&lang=en&sort_var=");
 
 							}
 
@@ -287,26 +297,110 @@ public class multiScraper extends Application {
 		return html.contains("BnQ");
 	}
 	//takes certain webpage as a html and ouputs bnq stuff
-	public Invoices readInvoices(String html) throws Exception
+	public EDA readBNQHTML(String html) throws Exception
 	{
-		String invoiceNo,purchOrNo,invDate,delDate,amountOut;
-		//CUSTOMER ORDER seems wrong
-		html = html.replace("\n", "").replace("\r", "");
-		invoiceNo = html.substring(html.indexOf("Invoice No") + 89, html.indexOf("&", html.indexOf("Invoice No")+89));
-		System.out.println(invoiceNo);
-		purchOrNo = html.substring(html.indexOf("Purchase Order No") + 96, html.indexOf("&", html.indexOf("Purchase Order No")+96));
-		System.out.println(purchOrNo);
-		invDate = html.substring(html.indexOf("Invoice Date") + 91, html.indexOf("&", html.indexOf("Invoice Date")+91));
-		invDate = invDate.replace(".", "/");
-		System.out.println(invDate);
-		delDate = html.substring(html.indexOf("Delivery Date") + 91, html.indexOf("&", html.indexOf("Delivery Date")+91));
+		//System.out.println(html);
+		String storeCode,purchOrderNo,custTellNo1,bQSuppNo,custName;
+		ArrayList<String> eanCode1 = new ArrayList<String>();
+		ArrayList<String> desc1 = new ArrayList<String>();
+		ArrayList<String> qty1 = new ArrayList<String>();
+		String dateOrderPlaced, delDate;
+		String salesNumber;
+		String custAdd1,custAdd2,custAdd3,custAdd4,custPostCode;
+
+		storeCode = html.substring(html.indexOf("hfStoreLocCode") + 25, html.indexOf(">", html.indexOf("hfStoreLocCode")+25)-1);
+		purchOrderNo = html.substring(html.indexOf("PURCHASE ORDER NO") + 162, html.indexOf("&", html.indexOf("PURCHASE ORDER NO")+162));
+		custTellNo1 = html.substring(html.indexOf("PHONE-DAY") + 166, html.indexOf("&", html.indexOf("PHONE-DAY")+166));
+		bQSuppNo = html.substring(html.indexOf("VENDOR") + 155, html.indexOf("&", html.indexOf("VENDOR")+155));
+		custName = html.substring(html.indexOf("ADDRESS - HOME DELIVERY") + 183, html.indexOf("&", html.indexOf("ADDRESS - HOME DELIVERY")+183));
+		dateOrderPlaced = html.substring(html.indexOf("PURCHASE ORDER DATE") + 164, html.indexOf("&", html.indexOf("PURCHASE ORDER DATE")+164));
+		dateOrderPlaced = dateOrderPlaced.replace(".", "/");
+		delDate = html.substring(html.indexOf("DELIVERY DATE") + 158, html.indexOf("&", html.indexOf("DELIVERY DATE")+158));
 		delDate = delDate.replace(".", "/");
-		System.out.println(delDate);
-		amountOut = html.substring(html.indexOf("Sub-Total") + 112, html.indexOf("&", html.indexOf("Sub-Total")+112));
-		System.out.println(amountOut);
-		
-		
-		return new Invoices(invoiceNo,purchOrNo,invDate,delDate,amountOut);
+		String custDetail = html.substring(html.indexOf("ADDRESS - HOME DELIVERY"), html.indexOf("CONTACT"));
+		salesNumber = html.substring(html.indexOf("SALES ORDER NO") + 160, html.indexOf("&", html.indexOf("SALES ORDER NO")+160));
+		String[] custDetailSplit = custDetail.split("<tr>");
+		String[] custInfo = new String[5]; 
+
+
+		//all the actual information 
+		for(int i = 2; i < 7; i++)
+		{
+			custInfo[i -2] = custDetailSplit[i].substring(103, custDetailSplit[i].indexOf("&",103));
+		}
+
+		//if the information is empty shift postcode
+		int numOfAddrLines = 4;
+		for(int i = 4;i >= 0; i--)
+		{
+			if(custInfo[i].equals(""))
+			{
+				numOfAddrLines = i - 1;
+			}
+		}
+
+		if(numOfAddrLines == 0)
+		{
+			custAdd1 = "";
+			custAdd2 = "";
+			custAdd3 = "";
+			custAdd4 = "";
+			custPostCode = custInfo[0];
+		}
+		else if(numOfAddrLines == 1)
+		{
+			custAdd1 = custInfo[0];
+			custAdd2 = "";
+			custAdd3 = "";
+			custAdd4 = "";
+			custPostCode = custInfo[1];
+		}
+		else if(numOfAddrLines == 2)
+		{
+			custAdd1 = custInfo[0];
+			custAdd2 = custInfo[1];
+			custAdd3 = "";
+			custAdd4 = "";
+			custPostCode = custInfo[2];
+		}
+		else if(numOfAddrLines == 3)
+		{
+			custAdd1 = custInfo[0];
+			custAdd2 = custInfo[1];
+			custAdd3 = custInfo[2];
+			custAdd4 = "";
+			custPostCode = custInfo[3];
+		}
+		else if(numOfAddrLines == 4)
+		{
+			custAdd1 = custInfo[0];
+			custAdd2 = custInfo[1];
+			custAdd3 = custInfo[2];
+			custAdd4 = custInfo[3];
+			custPostCode = custInfo[4];
+		}
+		else
+		{
+			System.out.println("No Address Lines?");
+			custAdd1 = "";
+			custAdd2 = "";
+			custAdd3 = "";
+			custAdd4 = "";
+			custPostCode = "";
+		}
+
+		//need to split by <!-- Begin Detail Line -->
+
+		String[] lines = html.split("<!-- Begin Detail Line -->");
+		for(int i = 1; i < lines.length; i ++)
+		{
+			eanCode1.add(lines[i].substring(lines[i].indexOf("EA&n") + 90, lines[i].indexOf("&", lines[i].indexOf("EA&n")+90))); 
+			desc1.add(lines[i].substring(lines[i].indexOf("EA&n") + 333, lines[i].indexOf("&", lines[i].indexOf("EA&n")+333)));
+			qty1.add(lines[i].substring(191, lines[i].indexOf("&",191)));
+
+		}
+
+		return new EDA(String.valueOf(seqNo), storeCode,  purchOrderNo,  custTellNo1, bQSuppNo,custName,  eanCode1,  desc1,  qty1,dateOrderPlaced,delDate,custAdd1,custAdd2,custAdd3,custAdd4,custPostCode,salesNumber);
 
 
 	}
@@ -319,13 +413,13 @@ public class multiScraper extends Application {
 		int page = scanner.nextInt();
 		System.out.println("Enter place on the page up to where you need (1- 10)");
 		int placeOnPage = scanner.nextInt();
+		System.out.println("Enter the next sequence number");
+		int seqNO = scanner.nextInt();
 
 		String[] myArg = new String[3];
 		myArg[0] = String.valueOf(page);
 		myArg[1] = String.valueOf(placeOnPage);
-		multiScraper.launch(myArg);
-
-
+		myArg[2] = String.valueOf(seqNO);
+		BNQMultiScraper.launch(myArg);
 	}
-
 }
